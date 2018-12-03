7af3bdbbcf6e47f93c3479656baf46d2d34c628c
leobene <leobene@192.168.0.100>
2018-12-03 14:59:45 -0200

    Fixed integraton test for company

--- tests/integration/models/company_test.py
+++ tests/integration/models/company_test.py
@@ -5,16 +5,16 @@ from tests.base_test import BaseTest
 class CompanyTest(BaseTest):
     def test_crud(self):
         with self.app_context():
-            competition = CompanyModel('GOL')
+            competition = CompanyModel('AVIANCA')
 
-            self.assertIsNone(CompanyModel.find_by_name('GOL'), "Found an competition with name 'GOL' before save_to_db")
+            self.assertIsNone(CompanyModel.find_by_name('AVIANCA').first(), "Found an competition with name 'AVIANCA' before save_to_db")
 
             competition.save_to_db()
 
-            self.assertIsNotNone(CompanyModel.find_by_name('GOL'),
-                                 "Did not find a competition with name 'GOL' after save_to_db")
+            self.assertIsNotNone(CompanyModel.find_by_name('AVIANCA').first(),
+                                 "Did not find a competition with name 'AVIANCA' after save_to_db")
 
             competition.delete_from_db()
 
-            self.assertIsNone(CompanyModel.find_by_name('GOL'), "Found an competition with name 'GOL' after delete_from_db")
+            self.assertIsNone(CompanyModel.find_by_name('AVIANCA').first(), "Found an competition with name 'AVIANCA' after delete_from_db")
 
