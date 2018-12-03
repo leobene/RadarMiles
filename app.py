cd67e2fcfdfbff74672fcc7b716a25b981baecb6
leobene <leobene@192.168.0.100>
2018-12-03 21:35:22 -0200

    Added a Blacklist

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
+from blacklist import BLACKLIST
 from resources.user import UserRegister, User, UserLogin, TokenRefresh
 from resources.company import Company, CompanyList
 
@@ -8,6 +9,8 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
+app.config['JWT_BLACKLIST_ENABLED'] = True  # enable blacklist feature
+app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']  # allow blacklisting for access and refresh tokens
 app.secret_key = 'RadarMiles'
 #app.config['JWT_SECRET_KEY'] =
 api = Api(app)
@@ -26,6 +29,11 @@ api.add_resource(TokenRefresh, '/refresh')
 #@app.errorhandler(JWTError)
 #def auth_error(err):
  #   return jsonify({'message': 'Could not authorize. Did you include a valid Authorization header?'}), 401
+
+# This method will check if a token is blacklisted, and will be called automatically when blacklist is enabled
+@jwt.token_in_blacklist_loader
+def check_if_token_in_blacklist(decrypted_token):
+    return decrypted_token['identity'] in BLACKLIST  # Here we blacklist particular users.
 
 # The following callbacks are used for customizing jwt response/error messages.
 @jwt.expired_token_loader
